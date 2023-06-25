1d02a1bcb6408cadd070d7777aa513a6dab8721f
winixt <haizekuo@gmail.com>
2023-06-25 15:02:42 +0800

    fix: 修复 baseURL 带域名的情况

--- src/middlewares/formatURL.ts
+++ src/middlewares/formatURL.ts
@@ -4,9 +4,13 @@ function isAbsoluteURL(url: string) {
   return /^([a-z][a-z\d+\-.]*:)?\/\//i.test(url)
 }
 
+function combineURLs(baseURL: string, relativeURL: string) {
+  return relativeURL ? `${baseURL.replace(/\/+$/, '')}/${relativeURL.replace(/^\/+/, '')}` : baseURL
+}
+
 export default async (ctx: Context, next: NextFn) => {
   if (ctx.config.baseURL && !isAbsoluteURL(ctx.config.url))
-    ctx.config.url = `${ctx.config.baseURL}/${ctx.config.url}`.replace(/\/{2,}/, '/')
+    ctx.config.url = combineURLs(ctx.config.baseURL, ctx.config.url)
 
   await next()
 }
