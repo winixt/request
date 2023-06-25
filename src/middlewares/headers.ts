3289401c6e70b497decc1a0fee4f99c49d4d1410
winixt <haizekuo@gmail.com>
2023-06-25 16:38:31 +0800

    fix: 修复 content-type 问题

--- src/middlewares/headers.ts
+++ src/middlewares/headers.ts
@@ -1,10 +1,14 @@
+import { isPlainObject } from 'lodash-es'
 import type { Context, NextFn } from '../interface'
 
 export default async (ctx: Context, next: NextFn) => {
   ctx.reqHeaders = new Headers(ctx.config.headers)
-  if (ctx.config.params instanceof FormData) {
-    if (!ctx.reqHeaders.has('Content-Type'))
+  if (!ctx.reqHeaders.has('Content-Type')) {
+    if (ctx.config.params instanceof FormData)
       ctx.reqHeaders.set('Content-Type', 'application/x-www-form-urlencoded')
+
+    else if (isPlainObject(ctx.config.params))
+      ctx.reqHeaders.set('Content-Type', 'application/json')
   }
 
   await next()
